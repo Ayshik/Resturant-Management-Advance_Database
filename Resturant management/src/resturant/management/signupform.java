03e01f2b047febd12d67ec3e60b1b2cf29a335c4
Mahmud <48443095+SwapnilMahmud@users.noreply.github.com>
2021-12-12 21:02:54 +0600

    e
    d

--- Resturant management/src/resturant/management/signupform.java
+++ Resturant management/src/resturant/management/signupform.java
@@ -388,6 +388,13 @@ public class signupform extends javax.swing.JFrame {
                 
         try
 		{
+                    if(this.jTextField1.getText().trim().isEmpty() || this.jTextField2.getText().trim().isEmpty() 
+                            || this.jTextField3.getText().trim().isEmpty() || this.jTextField5.getText().trim().isEmpty() 
+                            || this.jTextField6.getText().trim().isEmpty() || this.jComboBox1.getSelectedItem().toString().isEmpty()
+                            || this.dateChooserCombo3.getText().isEmpty() || this.jTextField3.getText().trim().isEmpty()){
+                        JOptionPane.showMessageDialog(null,"Fill all the fields");
+                  
+                }
 			 Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","XE","123");
               
                 PreparedStatement ps = conn.prepareStatement(query);
